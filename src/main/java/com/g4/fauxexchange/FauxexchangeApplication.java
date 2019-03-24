7ef7177320836b17b0b0a2fd4f507bb8e5f4bf47
arnie64 <arnold.m.dsilva@gmail.com>
2019-03-24 22:36:25 +1100

    Base code is in here. Application & Testing

---
+++ src/main/java/com/g4/fauxexchange/FauxexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.g4.fauxexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FauxexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FauxexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g4/fauxexchange/FauxexchangeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.g4.fauxexchange;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FauxexchangeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
