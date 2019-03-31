4a06f19b7c3f17d93226a8f036de78c1a826aa67
arnie64 <arnold.m.dsilva@gmail.com>
2019-03-31 19:01:05 +1100

    Begin Currency Development

---
+++ src/main/java/com/g4/fauxexchange/controller/CurrencyController.java
@@ -0,0 +1,22 @@
+package com.g4.fauxexchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CurrencyController {
+
+    @GetMapping("/template")
+    public String Template() {
+        return "template";
+    }
+
+	@RequestMapping("/notemplate") 
+	public String NoTemplate() {
+		return "notemplate";
+	}
+
+}
