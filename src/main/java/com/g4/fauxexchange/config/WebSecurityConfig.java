927e6d00467f33642f9901fed1bb640bc26f4ea6
arnie64 <arnold.m.dsilva@gmail.com>
2019-04-02 00:18:49 +1100

    Added the Permission for accessing api/currencies

--- src/main/java/com/g4/fauxexchange/config/WebSecurityConfig.java
+++ src/main/java/com/g4/fauxexchange/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/template", "/notemplate").permitAll()
+                .antMatchers("/", "/template", "/notemplate", "/api/currencies").permitAll()
                 .anyRequest().authenticated();
     }
 }
