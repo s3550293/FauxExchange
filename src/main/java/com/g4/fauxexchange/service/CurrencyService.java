f774891710ae3acedbb9475957535b6f435a2540
arnie64 <arnold.m.dsilva@gmail.com>
2019-04-04 20:00:15 +1100

    removed tabs :P

--- src/main/java/com/g4/fauxexchange/service/CurrencyService.java
+++ src/main/java/com/g4/fauxexchange/service/CurrencyService.java
@@ -4,8 +4,8 @@ import java.util.Collection;
 import com.g4.fauxexchange.model.Currency;
 
 public interface CurrencyService {
-	public abstract void createCurrency(Currency currency);
-	public abstract void updateCurrency();
-	public abstract void deleteCurrency(Currency currency);
-	public abstract Collection<Currency> getCurrencies();
+    public abstract void createCurrency(Currency currency);
+    public abstract void updateCurrency();
+    public abstract void deleteCurrency(Currency currency);
+    public abstract Collection<Currency> getCurrencies();
 }
