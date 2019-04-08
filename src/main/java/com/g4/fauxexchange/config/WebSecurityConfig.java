f84cc040b2dd599da30dce75da3ab04e0de7c889
arnie64 <arnold.m.dsilva@gmail.com>
2019-04-08 20:24:03 +1000

    Introduced new Security Paths

--- src/main/java/com/g4/fauxexchange/config/WebSecurityConfig.java
+++ src/main/java/com/g4/fauxexchange/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/template", "/notemplate", "/api/currencies").permitAll()
+                .antMatchers("/**", "/api/**", "/css/**","/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
