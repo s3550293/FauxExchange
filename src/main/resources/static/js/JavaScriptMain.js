16937e16f3a0cf0524a66ec049c9a78a6599c912
Joseph Garner <joseph.peter.garner@gmail.com>
2019-04-26 18:55:03 +1000

    Updated UI Layout Index

---
+++ src/main/resources/static/js/JavaScriptMain.js
@@ -0,0 +1,27 @@
+function showTrade(event){
+    document.getElementById("_trade").classList.add("show-text");
+}
+function hideTrade(event){
+    document.getElementById("_trade").classList.remove("show-text");
+}
+
+function showGrow(event){
+    document.getElementById("_grow").classList.add("show-text");
+}
+function hideGrow(event){
+    document.getElementById("_grow").classList.remove("show-text");
+}
+
+function showCompare(event){
+    document.getElementById("_compare").classList.add("show-text");
+}
+function hideCompare(event){
+    document.getElementById("_compare").classList.remove("show-text");
+}
+
+function showCompete(event){
+    document.getElementById("_compete").classList.add("show-text");
+}
+function hideCompete(event){
+    document.getElementById("_compete").classList.remove("show-text");
+}
