b289ef350ea9449cabbf81d1e39e23464ba6c3c6
Mark <mark.nguyen.9614@gmail.com>
2019-05-12 18:49:35 +1000

    Password confirmation works, trying to make the confirmation look nicer

--- src/main/resources/static/js/Register.js
+++ src/main/resources/static/js/Register.js
@@ -3,32 +3,63 @@ var i = 0;
 class Register extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            password:'',
+            passwordConfirm:''
+        }
     }
 
     registerHandleSubmit = (event) => {
         event.preventDefault();
-        const data = new FormData(event.target);
-        console.log(stringifyFormData(data));
-        fetch('/api/user/registration', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: stringifyFormData(data),
-        });
-        alert('Registration successful');
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+        if(this.state.password != this.state.passwordConfirm) {
+            alert('Passwords do not match');
+        }
+        else {
+            const data = new FormData(event.target);
+            console.log(stringifyFormData(data));
+            fetch('/api/user/registration', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: stringifyFormData(data),
+            });
+            alert('Registration successful');
+        }
+    }
+
+    updateInput = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+        console.log(this.state)
+    }
+
+    updateUpdatePassword = (event) => {
+        event.preventDefault();
+        if(this.state.password != this.state.passwordConfirm) {
+            event.target.setCustomValidity("Passwords do not match"); 
+        }
+        else {
+            event.target.setCustomValidity("");
+        }
     }
 
     render() {
         return(
             <div>
-                <form onSubmit = {this.registerHandleSubmit}>
-                    <input type="text" name="fName" required="required" placeholder="First Name" />
-                    <input type="text" name="lName" required="required" placeholder="Last Name" />
-                    <input type="date" name="dob" required="required" placeholder="Date of Birth" />
-                    <input type="email" name="email" required="required" placeholder="Email" />
-                    <input type="password" name="password" required="required" placeholder="Password" />
-                    <input type="submit" class="button" value="Register"/>
+                <form onSubmit = {this.registerHandleSubmit} onChange={this.updateInput}>
+                    <input type="text" id="fName" name="fName" required="required" placeholder="First Name" />
+                    <input type="text" id="lName" name="lName" required="required" placeholder="Last Name" />
+                    <input type="date" id="dob" name="dob" required="required" placeholder="Date of Birth" />
+                    <input type="email" id="email" name="email" required="required" placeholder="Email" />
+                    <input type="password" id = "password" name="password" required="required" placeholder="Password" />
+                    <input type="password"  id = "passwordConfirm" name="passwordConfirm" required="required" placeholder="Confirm your password"/>
+                    <input type="submit" className="button" value="Register"/>
                 </form>
             </div>
         );
