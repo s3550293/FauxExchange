db63db32eae39d9b9b65c2975eaf4fcc88009175
s3550293 <s3550293@student.rmit.edu.au>
2019-05-13 15:40:33 +1000

    Moved Desh over to React

--- src/main/resources/static/js/JavaScriptMain.js
+++ src/main/resources/static/js/JavaScriptMain.js
@@ -70,11 +70,11 @@ function devMode() {
     }
 }
 
-function uas() {
+function showUAS() {
     var isOn = $('#uas_Switch').is(':checked');
     if(isOn){
-
+        document.getElementById("UAS-Content").classList.remove("hideUAS");
     } else {
-        
+        document.getElementById("UAS-Content").classList.add("hideUAS");
     }
 }
