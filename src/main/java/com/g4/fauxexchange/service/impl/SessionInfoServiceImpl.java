39bd0423431eb1276dce65a93833b76367d2e340
arnie64 <arnold.m.dsilva@gmail.com>
2019-05-20 19:29:46 +1000

    Tell me if you find an error git message

---
+++ src/main/java/com/g4/fauxexchange/service/impl/SessionInfoServiceImpl.java
@@ -0,0 +1,47 @@
+package com.g4.fauxexchange.service;
+
+import java.util.List;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.g4.fauxexchange.model.SessionInfo;
+import com.g4.fauxexchange.model.User;
+import com.g4.fauxexchange.service.SessionInfoService;
+import com.g4.fauxexchange.dao.UserRepository;
+
+@Service
+public class SessionInfoServiceImpl implements SessionInfoService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public SessionInfo createSession(String sid, String email) {
+        SessionInfo si = new SessionInfo();
+        si.setSessionId(sid);
+        si.setUserEmail(email);
+        User user = userRepository.findByEmail(email);
+        System.out.println(user.getUserId());
+        si.setUserId(user.getUserId());
+
+        return si;
+    }
+
+    @Override
+    public SessionInfo createSession(HttpSession session) {
+        SessionInfo si = new SessionInfo();
+
+        si.setSessionId(session.getId());
+        si.setUserEmail((String)session.getAttribute("userEmail"));
+        si.setUserId((String)session.getAttribute("userId"));
+
+        return si;
+    }
+
+    @Override
+    public SessionInfo resetSesssion() {
+        return new SessionInfo();
+    }
+}
