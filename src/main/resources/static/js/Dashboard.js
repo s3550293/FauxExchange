ed8f5d05dca4f0936f418703c40b417ee39e6780
Joseph Garner <s3550293@student.rmit.edu.au>
2019-05-20 22:26:07 +1000

    Broke up render for dash

--- src/main/resources/static/js/Dashboard.js
+++ src/main/resources/static/js/Dashboard.js
@@ -16,123 +16,159 @@ class Dashboard extends React.Component {
         setTimeout(this.componentDidMount, 15000);
     }
 
+    userprofile(){
+        return(
+            <div className="pane pain-split-two profile">
+                <h4 className="box-icon" Style="font-size:104px;"><i className="fas fa-user"></i></h4>
+                <span>Name</span>
+                <span>Rank</span>
+                <span>Cash</span>
+            </div>
+        );
+    }
+
+    accountvalue(){
+        return(
+            <div className="pane pain-split-two account-value">
+                <h5>Account Value</h5>
+                <ul className="stats-list">
+                    <li>
+                        $2030 <span className="stats-list-label">Account Value</span>
+                    </li>
+                    <li className="stats-list-positive">
+                        $30 <span className="stats-list-label">Gains</span>
+                    </li>
+                    <li className="stats-list-negative">
+                        $0 <span className="stats-list-label">Loss</span>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+
+    holdings(){
+        return(
+            <div className="pane pain-split-two">
+                <h5>Invested Crypto</h5>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th className="text-center"></th>
+                            <th className="text-center">Code</th>
+                            <th className="text-center">Price</th>
+                            <th className="text-center">Qty</th>
+                            <th className="text-center">Value</th>
+                        </tr>
+                        <tr>
+                            <td className="text-center"></td>
+                            <td className="text-center">BTC</td>
+                            <td className="text-center">9000</td>
+                            <td className="text-center">3</td>
+                            <td className="text-center">27000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    orders(){
+        return(
+            <div className="pane pain-split-two">
+                <h5>Current orders</h5>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th className="text-center"></th>
+                            <th className="text-center">Code</th>
+                            <th className="text-center">Price</th>
+                            <th className="text-center">Qty</th>
+                            <th className="text-center">Value</th>
+                            <th className="text-center">Buy/Sell</th>
+                        </tr>
+                        <tr>
+                            <td className="text-center"></td>
+                            <td className="text-center">BTC</td>
+                            <td className="text-center">9010</td>
+                            <td className="text-center">1</td>
+                            <td className="text-center">9010</td>
+                            <td className="text-center">Sell</td>
+                        </tr>
+                        <tr>
+                            <td className="text-center"></td>
+                            <td className="text-center">BTC</td>
+                            <td className="text-center">8500</td>
+                            <td className="text-center">2</td>
+                            <td className="text-center">17000</td>
+                            <td className="text-center">Sell</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    leader(){
+        return(
+            <div className="pane pain-split-two">
+                <h5>Leader Board</h5>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th className="text-center">User</th>
+                            <th className="text-center">Value</th>
+                        </tr>
+                        <tr>
+                            <td className="text-center">Han Solo</td>
+                            <td className="text-center">$4000.00</td>
+                        </tr>
+                        <tr>
+                            <td className="text-center">Tony Stark</td>
+                            <td className="text-center">$3000.00</td>
+                        </tr>
+                        <tr>
+                            <td className="text-center">Natasha</td>
+                            <td className="text-center">$3.50</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>            
+        );
+    }
+
+    friends(){
+        return(
+            <div className="pane pain-split-two">
+                <h5>Friends</h5>
+                <form className="friends-search">
+                    <input className="seach-input" type="text"/>
+                    <input type="Submit" className="button" value="Search"/>
+                </form>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th className="text-center">Username</th>
+                            <th className="text-center">Value</th>
+                        </tr>
+                        <tr>
+                            <td className="text-center">Tony Stark</td>
+                            <td className="text-center">$3000.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
     render() {
         return(
             <div className="content-split-two">
-                <div className="pane pain-split-two profile">
-                    <h4 className="box-icon" Style="font-size:104px;"><i className="fas fa-user"></i></h4>
-                    <span>Name</span>
-                    <span>Rank</span>
-                    <span>Cash</span>
-                </div>
-                <div className="pane pain-split-two account-value">
-                    <h5>Account Value</h5>
-                    <ul className="stats-list">
-                        <li>
-                            $2030 <span className="stats-list-label">Account Value</span>
-                        </li>
-                        <li className="stats-list-positive">
-                            $30 <span className="stats-list-label">Gains</span>
-                        </li>
-                        <li className="stats-list-negative">
-                            $0 <span className="stats-list-label">Loss</span>
-                        </li>
-                    </ul>
-                </div>
-                <div className="pane pain-split-two">
-                    <h5>Invested Crypto</h5>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th className="text-center"></th>
-                                <th className="text-center">Code</th>
-                                <th className="text-center">Price</th>
-                                <th className="text-center">Qty</th>
-                                <th className="text-center">Value</th>
-                            </tr>
-                            <tr>
-                                <td className="text-center"></td>
-                                <td className="text-center">BTC</td>
-                                <td className="text-center">9000</td>
-                                <td className="text-center">3</td>
-                                <td className="text-center">27000</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="pane pain-split-two">
-                    <h5>Current orders</h5>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th className="text-center"></th>
-                                <th className="text-center">Code</th>
-                                <th className="text-center">Price</th>
-                                <th className="text-center">Qty</th>
-                                <th className="text-center">Value</th>
-                                <th className="text-center">Buy/Sell</th>
-                            </tr>
-                            <tr>
-                                <td className="text-center"></td>
-                                <td className="text-center">BTC</td>
-                                <td className="text-center">9010</td>
-                                <td className="text-center">1</td>
-                                <td className="text-center">9010</td>
-                                <td className="text-center">Sell</td>
-                            </tr>
-                            <tr>
-                                <td className="text-center"></td>
-                                <td className="text-center">BTC</td>
-                                <td className="text-center">8500</td>
-                                <td className="text-center">2</td>
-                                <td className="text-center">17000</td>
-                                <td className="text-center">Sell</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="pane pain-split-two">
-                    <h5>Leader Board</h5>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th className="text-center">User</th>
-                                <th className="text-center">Value</th>
-                            </tr>
-                            <tr>
-                                <td className="text-center">Han Solo</td>
-                                <td className="text-center">$4000.00</td>
-                            </tr>
-                            <tr>
-                                <td className="text-center">Tony Stark</td>
-                                <td className="text-center">$3000.00</td>
-                            </tr>
-                            <tr>
-                                <td className="text-center">Natasha</td>
-                                <td className="text-center">$3.50</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="pane pain-split-two">
-                    <h5>Friends</h5>
-                    <form className="friends-search">
-                        <input className="seach-input" type="text"/>
-                        <input type="Submit" className="button" value="Search"/>
-                    </form>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th className="text-center">Username</th>
-                                <th className="text-center">Value</th>
-                            </tr>
-                            <tr>
-                                <td className="text-center">Tony Stark</td>
-                                <td className="text-center">$3000.00</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                {this.userprofile()}
+                {this.accountvalue()}
+                {this.holdings()}
+                {this.orders()}
+                {this.leader()}
+                {this.friends()}
             </div>
         );
     }
